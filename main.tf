971eec6f778bd50b883d38be688edc644e0d24f4
VishalAerinI <vishal@aerinit.com>
2025-06-25 17:05:38 +0000

    main.tf file created

---
+++ main.tf
@@ -0,0 +1,250 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -- Lambda IAM Role --
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Effect    = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# -- Lambda Function --
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "example_lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  s3_bucket     = aws_s3_bucket.artifact_bucket.bucket
+  s3_key        = "lambda.zip"
+
+  publish      = true
+  timeout      = 10
+  memory_size  = 128
+  
+#   filename         = "lambda.zip"
+#   source_code_hash = filebase64sha256("lambda.zip")
+}
+
+# -- API Gateway --
+resource "aws_api_gateway_rest_api" "example_api" {
+  name        = "example-api"
+  description = "API Gateway for Lambda"
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
+  path_part   = "lambda"
+}
+
+resource "aws_api_gateway_method" "any_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.example_api.id
+  resource_id             = aws_api_gateway_resource.lambda_resource.id
+  http_method             = aws_api_gateway_method.any_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.example_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.example_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
+
+# -- Artifact Bucket --
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "artifact_bucket" {
+  bucket        = "artifact-bucket-${random_id.suffix.hex}"
+  force_destroy = true
+}
+
+# -- CodeBuild Role --
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-service-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "codebuild.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_codebuild_project" "example_build" {
+  name         = "example-build-project"
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:5.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = false
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+# -- CodePipeline Role --
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-service-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "codepipeline.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codepipeline_inline" {
+  name = "codepipeline-inline-policy"
+  role = aws_iam_role.codepipeline_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "s3:*",
+        "codebuild:StartBuild",
+        "codebuild:BatchGetBuilds",
+        "lambda:InvokeFunction",
+        "iam:PassRole",
+        "codestar-connections:UseConnection"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+# -- CodePipeline --
+resource "aws_codepipeline" "example_pipeline" {
+  name     = "example-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifact_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration = {
+        ConnectionArn    = "arn:aws:codeconnections:us-east-1:975050191688:connection/30711670-72ef-46d5-9c9e-e89e98578f76"
+        FullRepositoryId = "VishalAerinIt/lambda-function"
+        BranchName       = "master"
+        DetectChanges    = "true"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+    action {
+      name             = "BuildAction"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      configuration = {
+        ProjectName = aws_codebuild_project.example_build.name
+      }
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role_policy" "codebuild_logging_policy" {
+  name = "codebuild-logging-policy"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = [
+          "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/codebuild/*",
+          "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/codebuild/*:log-stream:*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*",.
+          "lambda:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+output "api_gateway_invoke_url" {
+  description = "The invoke URL of the deployed API Gateway stage"
+  value       = "https://${aws_api_gateway_rest_api.example_api.id}.execute-api.us-east-1.amazonaws.com/${aws_api_gateway_stage.dev.stage_name}/lambda"
+}
